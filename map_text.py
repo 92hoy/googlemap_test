587d8aa1b000be38430e348e5f43cb910a565d3e
jhy <jhy@jhys-iMac.local>
2020-12-03 15:13:21 +0900

    testcase2

--- map_text.py
+++ map_text.py
@@ -1,37 +1,45 @@
 import pandas as pd
 import gmplot 
-df1 = pd.read_excel("AI_TEST_201104.xlsx")
-df1.head()
-print (df1)
+df = pd.read_excel("AI_TEST_201104.xlsx",sheet_name = '03_자동측정망')
+# print (df1['이름'])
+# print (df1['TM_XAXS'])
+# print (df1['TM_YAXS'])
+# print (df1['SN'])
+# print (df1['N_SN'])
+df1 = df['SN'] != 0
+df2 = df['N_SN'] != 0
+df1 = df[df1]
+df2 = df[df2]
 
-latitude_list = [ 37.658, 37.877, 37.955] 
-longitude_list = [ 127.379, 127.678, 127.685 ] 
-gmap3 = gmplot.GoogleMapPlotter(37.658, 
-                                127.379, 13) 
-  
+X1=[]
+Y1=[]
+for i in df1['TM_XAXS']:
+    X1.append(i)
 
-gmap3.scatter( latitude_list, longitude_list, '#FF0000', 
-                              size = 40, marker = True ) 
+for i in df1['TM_YAXS']:
+    Y1.append(i)
+
+X2=[]
+Y2=[]
+for i in df2['TM_XAXS']:
+    X2.append(i)
+
+for i in df2['TM_YAXS']:
+    Y2.append(i)
+latitude_list1 = X1
+longitude_list1 = Y1
+
+latitude_list2 = X2
+longitude_list2 = Y2
+
+gmap1 = gmplot.GoogleMapPlotter(37.87691340116111, 127.6780014421773, 13)
+gmap2 = gmplot.GoogleMapPlotter(37.47683764223752, 127.35473560405816, 13)
   
-# Plot method Draw a line in 
-# between given coordinates 
-gmap3.plot(latitude_list, longitude_list,  
-           'cornflowerblue', edge_width = 2.5) 
-# # import gmplot package 
-# import gmplot 
+gmap1.scatter( latitude_list1, longitude_list1, '#FF0000',size = 40, marker = True ) 
+gmap1.plot(latitude_list1, longitude_list1,'cornflowerblue', edge_width = 2.5) 
+gmap1.draw('map1.html')
+
+gmap2.scatter( latitude_list2, longitude_list2, '#FFFFFF',size = 40, marker = True ) 
+gmap2.plot(latitude_list2, longitude_list2,'cornflowerblue', edge_width = 2.5) 
+gmap2.draw('map2.html')
   
-# latitude_list = [ 37.658, 37.877, 37.955] 
-# longitude_list = [ 127.379, 127.678, 127.685 ] 
-  
-# gmap3 = gmplot.GoogleMapPlotter(38.098, 127.709, 13) 
-  
-# # scatter method of map object  
-# # scatter points on the google map 
-# gmap3.scatter( latitude_list, longitude_list, '#FF0000',size = 40, marker = False ) 
-  
-# # Plot method Draw a line in 
-# # between given coordinates 
-# gmap3.plot(latitude_list, longitude_list,  
-#            'cornflowerblue', edge_width = 2.5) 
-  
-gmap3.draw('map2.html')
